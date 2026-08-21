8dfe81fce995799f7812a5fc33bb173550eed9ca
nathnael-chilot <chilotnathnael@gmail.com>
2026-08-21 16:14:51 +0300

    Implement Comment and Post viewsets with author assignment on creation

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
