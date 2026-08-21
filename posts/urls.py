69b7877c82fdfe4cb4df6f92352c1d069cf42dcd
nathnael-chilot <chilotnathnael@gmail.com>
2026-08-21 16:55:49 +0300

    Add API URL routing for posts and comments

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+# posts/urls.py
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='post')
+
+urlpatterns = router.urls
