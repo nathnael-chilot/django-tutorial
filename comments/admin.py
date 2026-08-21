fe474f94e1e66c1dd2a316bf6af5621f70917571
nathnael-chilot <chilotnathnael@gmail.com>
2026-08-21 13:00:44 +0300

    Register Comment and Post models in the Django admin with custom configurations

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'body', 'created_at')
+    list_filter = ('post', 'author')
+    search_fields = ('body',)
+    ordering = ('-created_at',)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Category
 
-# Register your models here.
+admin.site.register(Category)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'published', 'created_at', 'updated_at')
+    list_filter = ('published', 'category', 'author')
+    search_fields = ('title', 'content')
+    ordering = ('-created_at',)
